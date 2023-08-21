2138a8be8066ed2e910a700303b72d915fa31cf2
nezarcyy <n.as09391259@gmail.com>
2023-08-21 18:16:54 +0100

    ga-integration

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,3 +1,4 @@
+import useAnalyticsEventTracker from "./integrations/useAnalyticsEventTracker";
 import { Link as ScrollLink } from "react-scroll";
 import { useEffect } from "react";
 import '../App.css';
@@ -16,6 +17,8 @@ const Hero = () => {
     }, 40);
   }, []);
 
+  const gaEventTracker = useAnalyticsEventTracker('Subscription Click');
+
 
   return (
     <div className="bg-gradient-to-t from-black/40">
@@ -32,7 +35,7 @@ const Hero = () => {
         </p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <ScrollLink to="contact" smooth={true} duration={900}>
-            <button href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-gradient-to-r from-yellow-400 to-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-400 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <button href="#" onClick={()=>gaEventTracker('sub')} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-gradient-to-r from-yellow-400 to-yellow-300 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-400 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
 
               Subscribe Now!
 
