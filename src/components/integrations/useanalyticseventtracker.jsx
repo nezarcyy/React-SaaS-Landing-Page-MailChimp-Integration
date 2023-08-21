dd4bb84afb69e91f45e90ac483f40540666ec27e
nezarcyy <n.as09391259@gmail.com>
2023-08-21 18:33:53 +0100

    ga

---
+++ src/components/integrations/useanalyticseventtracker.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactGA from "react-ga";
+
+const useAnalyticsEventTracker = (category="Landing-Page") => {
+  const eventTracker = (action = "Subscribe", label = "Subscription") => {
+    ReactGA.event({category, action, label});
+  }
+  return eventTracker;
+}
+export default useAnalyticsEventTracker;
